576c5246067bd7e2cded0b1d56ced5bbdbb237a9
pauloho97 <160677290+pauloho97@users.noreply.github.com>
2026-03-21 04:19:40 +0000

    continuar programando em coleta para token

--- app/coleta.py
+++ app/coleta.py
@@ -1,31 +1,66 @@
 import requests
 
-def buscar_dados():
-    url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.1/dados?formato=json&dataInicial=01/01/2023"
+TOKEN = "e195d64f4cc21c0b139620372db87c73"
 
-    try:
-        response = requests.get(url)
+def testar_token():
+    url = "https://api.portaldatransparencia.gov.br/api-de-dados/orgaos"
 
-        print("Status code:", response.status_code)  # 👈 debug
+    headers = {
+        "chave-api-dados": TOKEN,
+        "Accept": "application/json"
+    }
 
-        if response.status_code == 200:
-            return response.json()
-        else:
-            print("Erro:", response.text)
-            return None
+    response = requests.get(url, headers=headers)
 
-    except Exception as e:
-        print("Erro na requisição:", e)
-        return None
+    print(response.request.headers)
+    
+    print("Status code:", response.status_code)
+
+    if response.status_code == 200:
+        dados = response.json()
+        print("Token funcionando ✔️")
+        print(dados[:2])  # mostra 2 registros
+    else:
+        print("Erro:", response.text)
 
 def main():
-    dados = buscar_dados()
-
-    if dados:
-        print("Dados recebidos:")
-        print(dados[:5])
-    else:
-        print("Nenhum dado retornado")
+    testar_token()
 
 if __name__ == "__main__":
     main()
+
+#def buscar_gastos():
+ #   url = "https://api.portaldatransparencia.gov.br/api-de-dados/orgaos"
+  #  headers = {
+   #     "chave-api-dados": TOKEN,
+    #    "Accept": "application/json"
+  #  }
+#
+ #   params = {
+  #  "ano": 2024,
+   # "mes": 1,
+   # "codigoFuncao": "12",
+   # "pagina": 1
+  #  }
+
+   # response = requests.get(url, headers=headers, params=params)
+#
+ #   print("Status code:", response.status_code)
+#
+  #  if response.status_code == 200:
+ #       return response.json()
+  #  else:
+   #     print("Erro:", response.text)
+    #    return None
+
+#def main():
+ #   dados = buscar_gastos()
+#
+ #   if dados:
+  #      print("Dados recebidos:")
+   #     print(dados[:2])  # mostra 2 registros
+   # else:
+    #    print("Nenhum dado retornado")
+
+#if __name__ == "__main__":
+ #   main()
